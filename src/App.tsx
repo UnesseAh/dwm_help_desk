a36256ef9026b338c071d51e19c54975ea57ced4
soufianerami-dev <soufianeramip@gmail.com>
2026-06-04 18:03:32 +0000

    feat: complete admin interface prototype

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider } from "@/components/ui/theme-provider"
 import { AppLayout } from "@/components/layout/AppLayout"
 import { TicketsList } from "@/pages/Tickets/TicketsList"
 import { Dashboard } from "@/pages/Dashboard"
+import { UsersList } from "@/pages/Users/UsersList"
+import { SettingsPage } from "@/pages/Settings/SettingsPage"
 
 function App() {
   return (
@@ -12,8 +14,8 @@ function App() {
           <Route path="/" element={<AppLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="tickets" element={<TicketsList />} />
-            <Route path="users" element={<div className="p-4">Users Page coming soon...</div>} />
-            <Route path="settings" element={<div className="p-4">Settings coming soon...</div>} />
+            <Route path="users" element={<UsersList />} />
+            <Route path="settings" element={<SettingsPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
